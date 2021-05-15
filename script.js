bed083bb3d7adf62f606e04ed4487b5de1603422
Renato Magrini <renatomagrini@gmail.com>
2021-05-15 14:19:21 +0200

    modal window style, open and close working

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log("test");
+// TAGS
+
+const saveBtn = document.querySelector(".save");
+const cancelBtn = document.querySelector(".cancel");
+const modalWindow = document.querySelector(".modal-overlay");
+
+const Modal = {
+  open() {
+    modalWindow.classList.add("active");
+  },
+  close() {
+    modalWindow.classList.remove("active");
+  },
+};
